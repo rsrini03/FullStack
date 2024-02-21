62ae9d1a9bb9962bef11cf9b940b34250841406e
rsrini03 <rsrini169@gmail.com>
2024-02-21 14:35:09 +0530

    changes

---
+++ Day1/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './Hero'
+import Features from './Features'
+import Ratings from './Ratings'
+
+const Home = () => {
+    return (
+        <div>
+           <Hero/>
+           <Features/>
+           <Ratings/>
+        </div>
+    )
+}
+
+export default Home
